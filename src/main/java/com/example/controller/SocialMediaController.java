6f48b73dc2dc6cc577e3cb53f75322d876f57142
Benjamin Huh <benjaminhuh4@gmail.com>
2024-11-11 05:58:55 +0000

    Initial Commit

--- src/main/java/com/example/controller/SocialMediaController.java
+++ src/main/java/com/example/controller/SocialMediaController.java
@@ -1,5 +1,9 @@
 package com.example.controller;
 
+import org.springframework.*;
+import org.springframework.stereotype.Controller;
+import com.example.service.*;
+
 
 /**
  * TODO: You will need to write your own endpoints and handlers for your controller using Spring. The endpoints you will need can be
@@ -7,6 +11,11 @@ package com.example.controller;
  * where applicable as well as the @ResponseBody and @PathVariable annotations. You should
  * refer to prior mini-project labs and lecture materials for guidance on how a controller may be built.
  */
+@Controller
 public class SocialMediaController {
+    private MessageService messageService;
+    private AccountService accountService;
+
+
 
 }
