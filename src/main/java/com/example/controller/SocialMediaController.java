e8f4bb633e58ba07c1e62c748bdd11f650fb6a84
Benjamin Huh <benjaminhuh4@gmail.com>
2024-11-12 15:04:24 +0000

    outlined controller methods

--- src/main/java/com/example/controller/SocialMediaController.java
+++ src/main/java/com/example/controller/SocialMediaController.java
@@ -1,8 +1,17 @@
 package com.example.controller;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.tomcat.jni.User;
 import org.springframework.*;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+
+import org.springframework.web.bind.annotation.*;
+
 import com.example.service.*;
+import com.example.entity.*;
 
 
 /**
@@ -11,11 +20,81 @@ import com.example.service.*;
  * where applicable as well as the @ResponseBody and @PathVariable annotations. You should
  * refer to prior mini-project labs and lecture materials for guidance on how a controller may be built.
  */
-@Controller
+@RestController
 public class SocialMediaController {
     private MessageService messageService;
     private AccountService accountService;
 
 
+
+    //Handler for registering a new user
+    //Returns the new user inserted given that the username isn't empty, does not already exist in the repository, 
+    //and password is >4 characters. 
+    //Duplicate username: Status 409 else: Status 400
+    @PostMapping("/register")
+    public ResponseEntity<User> postNewUserHandler(@RequestBody Account account){
+        User registeredUser = accountService.registerAccount(account);
+        
+        return null;
+    }
+
+    //Handler for verifying login given json account
+    //Logged in: Status 200 Unsuccessful login: Status 401
+    //Return a logged in user if successful.
+    @PostMapping("/login")
+    public User postUserLoginHandler(@RequestBody Account account){
+
+        return null;
+    }
+
+    //Handler for making a new post given json message, excluding id
+    //Successful if body isn't blank, isn't over 255 chars, postedBy User exists in repository
+    //Successful Status: 200 Unsuccessful Status: 400
+    //Return a json message including id
+    @PostMapping("/messages")
+    public Message postMessageHandler(@RequestBody Message message){
+        return null;
+    }
+
+    //Handler for getting all messages
+    //Status: 200
+    //Return a list of messages, empty if repository is empty
+    @GetMapping("/messages")
+    public List<Message> getAllMessagesHandler(){
+        return new ArrayList<Message>();
+    }
+
+    //Handler for getting a message based on an id
+    //Status: 200
+    //Return message picked, if id doesn't match, return an empty response
+    @GetMapping("/messages/{messageId}")
+    public Message getMessageByIdHandler(@PathVariable int messageId){
+        return null;
+    }   
+
+    //Handler for deleting a message based on id
+    //Status: 200
+    //Given the id, delete the message, and return the number of rows (entries) updated
+    @DeleteMapping("/messages/{messageId}")
+    public int deleteMessageByIdHandler(@PathVariable int messageId){
+        return 0;
+    }
+
+    //Handler for updating a message based on id
+    //Successful Status: 200, Failed status: 400
+    //Given the id and new message, update if text is present and under 255 chars.
+    //Return rows updated
+    @PatchMapping("/messages/{messageId}")
+    public int patchMessageByIdHandler(@PathVariable int messageId){
+        return 0;
+    }
+
+    //Handler for getting all messages based on the user that posted them
+    //Status: 200
+    //Given the user's id, Return a list of all messages with that user's id in postedBy
+    @GetMapping("/accounts/{accountId}/messages")
+    public List<Message> getMessageByUserHandler(@PathVariable int accountId){
+        return new ArrayList<>();
+    }
 
 }
--- src/main/java/com/example/service/AccountService.java
+++ src/main/java/com/example/service/AccountService.java
@@ -1,7 +1,17 @@
 package com.example.service;
 
+import org.apache.tomcat.jni.User;
 import org.springframework.stereotype.Service;
+
+import com.example.entity.Account;
+import com.example.repository.AccountRepository;
 
 @Service
 public class AccountService {
+    AccountRepository repository;
+
+    public User registerAccount(Account account) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'registerAccount'");
+    }
 }
